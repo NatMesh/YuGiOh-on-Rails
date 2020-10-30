0714256c92d8b984fa00132b598d6767b93ef006
Nathanael <nathanael.meshesha@yahoo.com>
2020-10-30 05:30:15 -0500

    Implemented link marker directions in the Card show page for our link monster cards.

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,4 +2,8 @@ class Card < ApplicationRecord
   has_many :monster_cards
   validates :name, :card_type, :description, :race, :card_image, :card_image_small, presence: true
   validates :name, uniqueness: true
+
+  def markers_list
+    monster_cards.first.markers.map(&:marker_direction).join(", ")
+  end
 end
