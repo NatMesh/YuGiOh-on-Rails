4ccfde1f715f1d8aa6e302b470a2095f28c25862
Nathanael <nathanael.meshesha@yahoo.com>
2020-10-30 07:43:34 -0500

    Implemented basic search functionality.

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -20,4 +20,9 @@ class CardsController < ApplicationController
   end
   #The @card variable will be shared with:
   #app/views/cards/show.html.erb
+
+  def search
+    wildcard_search = "%#{params[:keywords]}%"
+    @cards = Card.where("name LIKE ?", wildcard_search)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :about, only: %i[index]
-  resources :cards, only: %i[index show]
+  resources :cards, only: %i[index show] do
+    collection do
+      get "search"
+    end
+  end
   root to: "home#index"
 
 end
